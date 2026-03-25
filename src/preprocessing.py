cafc6666f535d7d9ded3763072ef391534814023
Cabrel GANGBE <gangbecabrel@gmail.com>
2026-03-25 05:15:15 +0100

    Modification of the library preprocessing

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -8,5 +8,6 @@ def wrangle(path):
     df.drop(columns=df.columns[0], inplace=True) # remove an empty column
     df.drop_duplicates(inplace=True) # remove duplicates
     df[df.columns[3]] = pd.to_datetime(df[df.columns[3]], format='ISO8601', utc=True) # conversion in timestamp
+    df.set_index(df.columns[3], inplace=True) # set timestamp in index
 
     return df
